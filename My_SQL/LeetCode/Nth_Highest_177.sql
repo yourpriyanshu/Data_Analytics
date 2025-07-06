2103e73c76e93ca424c73541b8d71b064cbacdff
Priyanshu Kumar <priyanshukumarkota@gmail.com>
2025-07-07 00:49:25 +0530

    Leetcode 177 Nth Highest Salary

--- My_SQL/LeetCode/Nth_Highest_177.sql
+++ My_SQL/LeetCode/Nth_Highest_177.sql
@@ -1,7 +1,13 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+  DECLARE offset_val INT;
+  SET offset_val=N-1;
   RETURN (
       # Write your MySQL query statement below.
+      SELECT DISTINCT Salary 
+      FROM Employee
+      ORDER BY Salary DESC
+      LIMIT 1 OFFSET offset_val
 
   );
 END
